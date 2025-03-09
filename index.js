ef6626c9a049612b0d0b96f10eafa8bc88c03d20
iynulwa <joshuaiyanuoluwa1@gmail.com>
2025-03-09 21:57:39 +0100

    HTML corrections and JS modified by looping over buttons using forEach methood

--- index.js
+++ index.js
@@ -1,64 +1,25 @@
 let isOpened = false;
 
-document.querySelectorAll("h2")[0].addEventListener("click", function() {
-
-    isOpened = !isOpened;
-
-    if (!isOpened) {
-        document.getElementById("minus1").style.display = "block";
-        document.getElementById("plus1").style.display = "none";
-        document.querySelector("p").style.display = "block";
-    } else {
-        document.getElementById("minus1").style.display = "none";
-        document.getElementById("plus1").style.display = "block";
-        document.querySelector("p").style.display = "none";
-    }
-})
+const allButtons = document.querySelectorAll("button");
+const contents = document.getElementsByClassName("content");
+const theMinus = document.getElementsByClassName("minus");
+const thePlus = document.getElementsByClassName("plus");
 
 
-document.querySelectorAll("h2")[1].addEventListener("click", function() {
+allButtons.forEach(function(element, index) {
+    element.addEventListener("click", function() {
 
-    isOpened = !isOpened;
+        if(!isOpened) {
+            contents[index].style.display="block";
+            theMinus[index].style.display="block";
+            thePlus[index].style.display="none"
+        } else {
+            contents[index].style.display="none";
+            theMinus[index].style.display="none";
+            thePlus[index].style.display="block"
+        }
 
-    if (!isOpened) {
-        document.getElementById("minus2").style.display = "block";
-        document.getElementById("plus2").style.display = "none";
-        document.querySelectorAll("p")[1].style.display = "block";
-    } else {
-        document.getElementById("minus2").style.display = "none";
-        document.getElementById("plus2").style.display = "block";
-        document.querySelectorAll("p")[1].style.display = "none";
-    }
-})
-
-
-document.querySelectorAll("h2")[2].addEventListener("click", function() {
-
-    isOpened = !isOpened;
-
-    if (!isOpened) {
-        document.getElementById("minus3").style.display = "block";
-        document.getElementById("plus3").style.display = "none";
-        document.querySelectorAll("p")[2].style.display = "block";
-    } else {
-        document.getElementById("minus3").style.display = "none";
-        document.getElementById("plus3").style.display = "block";
-        document.querySelectorAll("p")[2].style.display = "none";
-    }
-})
-
-
-document.querySelectorAll("h2")[3].addEventListener("click", function() {
-
-    isOpened = !isOpened;
-
-    if (!isOpened) {
-        document.getElementById("minus4").style.display = "block";
-        document.getElementById("plus4").style.display = "none";
-        document.querySelectorAll("p")[3].style.display = "block";
-    } else {
-        document.getElementById("minus4").style.display = "none";
-        document.getElementById("plus4").style.display = "block";
-        document.querySelectorAll("p")[3].style.display = "none";
-    }
+        isOpened = !isOpened;
+        
+    })
 })
