3726d6ad10e12317aa9c3c681f715c7c97b024da
iynulwa <joshuaiyanuoluwa1@gmail.com>
2025-03-20 11:59:10 +0100

    aria labels removed, aria expanded correction, header removal, button placed inside h3 instead of vice-versa

--- index.js
+++ index.js
@@ -12,11 +12,13 @@ allButtons.forEach(function(element, index) {
         if(!isOpened) {
             contents[index].style.display="block";
             theMinus[index].style.display="block";
-            thePlus[index].style.display="none"
+            thePlus[index].style.display="none";
+            this.ariaExpanded="true"
         } else {
             contents[index].style.display="none";
             theMinus[index].style.display="none";
-            thePlus[index].style.display="block"
+            thePlus[index].style.display="block";
+            this.ariaExpanded="false"
         }
 
         isOpened = !isOpened;
